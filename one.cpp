aaae54059648b435486bffef58e73545a1e3db63
Tushar Saxena <stushar345@gmail.com>
2021-01-17 00:23:24 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,26 @@
+    string commonPrefixUtil(string str1, string str2) 
+{ 
+    string result; 
+    int n1 = str1.length(), n2 = str2.length(); 
+  
+    for (int i=0, j=0; i<=n1-1&&j<=n2-1; i++,j++) 
+    { 
+        if (str1[i] != str2[j]) 
+            break; 
+        result.push_back(str1[i]); 
+    } 
+  
+    return result; 
+} 
+  
+string longestCommonPrefix  (string arr[], int n) 
+{ 
+    string prefix =  arr[0]; 
+  
+    for (int i=1; i<=n-1; i++) 
+        prefix = commonPrefixUtil(prefix, arr[i]); 
+  
+  if(prefix=="")
+  return "-1";
+    return prefix; 
+} 
